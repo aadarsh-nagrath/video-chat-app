f862922d040dfadb865b5316c77f2a4679bf0070
Aadarsh Nagrath <anagrath1@gmail.com>
2023-08-30 02:06:44 +0530

    PeerService.js - RTC connection & accepting call
    -PeerService.js - RTC connection
    -Calling - incomming call - Acepting call
    -Accepting call feature added ( when accepted caller stream is recieved)

--- client/src/pages/Room.jsx
+++ client/src/pages/Room.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useCallback, useState} from "react";
 import { useSocket } from "../context/socketProvider";
 //We will create listeners that will tell us that users have joined this room
 import ReactPlayer from "react-player";
+import peer from "../services/peer";
 
 const RoomPage = () => {
     const socket = useSocket();
@@ -14,26 +15,53 @@ const RoomPage = () => {
     }, []);
 
     const handleCall = useCallback(async ()=>{
-    const stream = await navigator.mediaDevices.getUserMedia(
+        const stream = await navigator.mediaDevices.getUserMedia(
+            {
+                audio:true, 
+                video: true
+            });
+
+            const offer = await peer.getOffer();
+            socket.emit('user:call', {to: remoteSocketId, offer});
+            setMyStream(stream);
+        }, [remoteSocketId, socket]);
+    //install react player 
+
+    const handleIncommingCall = useCallback(async (from, offer)=>{
+        setRemoteSocketId(from);
+        // turning on connecting user's stream
+        const stream = await navigator.mediaDevices.getUserMedia(
         {
             audio:true, 
             video: true
         });
-
         setMyStream(stream);
+
+        console.log(`Incomming call`, from, offer);
+        const ans = await peer.getAnswer(offer);
+        socket.emit('call:accepted', {to: from, ans})
+    }, [socket]);
+
+    const handleCallAccept = useCallback(({from, ans})=>{
+        // whenever Our call is accepted we have to store it in our localDescription
+        peer.setLocalDescription(ans);
+        console.log("Call Accepted");
     }, []);
-    //install react player 
 
 
     useEffect(() => {
     // Register the event listener when the component mounts
         socket.on("user:joined", handleUserJoined);
+        socket.on("incomming:call", handleIncommingCall);
+        socket.on("call:accepted", handleCallAccept);
 
     // Unregister the event listener when the component unmounts
         return () => {
             socket.off("user:joined", handleUserJoined);
+            socket.off("incomming:call", handleIncommingCall);
+            socket.off("call:accepted", handleCallAccept);
         };
-        }, [socket, handleUserJoined]);
+        }, [socket, handleUserJoined, handleIncommingCall, handleCallAccept]);
 
     return (
     <div>
@@ -48,6 +76,7 @@ const RoomPage = () => {
             <h1>My Stream</h1>
             <ReactPlayer playing muted height="300px" width="500px" url={myStream} />
         </>
+        //Now to send our stream to other users we will create services folder
         )
     }
     </div>
